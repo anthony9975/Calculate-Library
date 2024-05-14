5a080ef15e8847f6e11455ae76119813bada8ce0
antho <antho@10.0.0.9>
2024-05-13 19:40:21 -0700

    Initial Commit.

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+/*Anthony Ma
+ *August 31, 2023
+ *Client code, used to run the math library
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.quadForm(1,-4,2));
+
+	}
+
+}
